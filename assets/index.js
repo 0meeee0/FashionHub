8df4ac8b3dc1e5e7e03e08e80a8c3795ed8c2d71
0meeee0 <mehdi74.id@gmail.com>
2023-11-02 14:49:28 +0100

    w

--- assets/index.js
+++ assets/index.js
@@ -5,42 +5,46 @@ for (let i = 0; i < faq.length; i++) {
         alert('message pour faq');
     });
 }
+const tmenu = document.querySelector(".navbar-conatiner-burger");
+const dmenu = document.getElementById("links");
+
+ function onHumberger(){
+if( dmenu.classList.contains("show"))
+    dmenu.classList.remove("show")
+else
+    dmenu.classList.add("show")
+}
 
 function validateForm() {
-    // Get values from input fields (username, email, and password)
     var username = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var phonenumber = document.getElementById("phonenumber").value;
-    // Regex patterns for email and password validation
+
     var emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
     var passwordPattern = /^(?=.[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    var phonePattern = /^\(\d{3}\) \d{3}-\d{4}$/;
-    // Validate email using regex pattern
+    var phonePattern = /^\d{10}$/; 
+console.log(username)
     if (!emailPattern.test(email)) {
       alert("Invalid email address!");
-      return false; 
+      return ; 
     }
 
-    // Validate password using regex pattern
     if (!passwordPattern.test(password)) {
       alert(
         "Password must be at least 8 characters long and contain at least one letter and one number."
-      ); // Show an alert if password is not valid
-      return false; // Prevent form submission
+      );
+      return ;
     }
 
-     // Validate phone number using regex pattern
      if (!phonePattern.test(phonenumber)) {
         alert("Invalid phone number. Use the format (xxx) xxx-xxxx.");
-        return false;
-    }
-    // Check if the username is empty
-    if (username === "") {
-      alert("Username is required!"); // Show an alert if username is empty
-      return false; // Prevent form submission
+        return ;
     }
 
-    // If all validations pass, the form is considered valid and can be submitted
-    return true;
+    if (username === "") {
+      alert("Username is required!");
+    }
+
+     return true;
   }
